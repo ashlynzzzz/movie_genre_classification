9bf3a8440577d4b6d36500354a84a7fc399920c4
cindy-shaw <118010346@link.cuhk.edu.cn>
2023-11-24 16:03:27 -0500

    update movie.csv

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,17 +1,46 @@
+import ssl
+# Disable SSL certificate verification
+ssl._create_default_https_context = ssl._create_unverified_context
+
+import re
 import pandas as pd
 from functools import partial
 from sklearn.preprocessing import MultiLabelBinarizer
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+stop_words_eng = set(stopwords.words('english'))
+stop_words_chi = set(stopwords.words('chinese'))
 
 
-def clean(x):
+def clean_text_chinese(text):
     """
     Eliminate redundant \n, space and <br/> from Chinese movie overviews.
     """
     characters_to_remove = ['\n', '\u3000', '<br/>']
-    modified_string = x
     for char in characters_to_remove:
-        modified_string = modified_string.replace(char, '')
-    return modified_string
+        text = text.replace(char, '')
+
+    # remove whitespaces 
+    text = ' '.join(text.split()) 
+    return text
+
+def clean_text_english(text):
+    # remove backslash-apostrophe 
+    text = re.sub('\'', '', text) 
+    # remove everything except alphabets 
+    text = re.sub('[^a-zA-Z]', ' ', text) 
+    # remove whitespaces 
+    text = ' '.join(text.split()) 
+    # convert text to lowercase 
+    text = text.lower() 
+    return text
+
+
+def remove_stopwords(text, language):
+    stop_words = stop_words_chi if language == 'chinese' else stop_words_eng
+    no_stopword_text = [w for w in text.split() if not w in stop_words]
+    return ' '.join(no_stopword_text)
 
 def specialize(id, language):
     """
@@ -36,20 +65,23 @@ def english_genre_transform(s):
     return s.split(',')
 
 def mian():
-    chinese = pd.read_csv('chinese_movies.csv')
-    english = pd.read_csv('english_movies.csv', engine='python')
+    chinese = pd.read_csv('data/chinese_movies.csv')
+    english = pd.read_csv('data/english_movies.csv', engine='python')
     chinese.dropna(inplace=True)
     english.dropna(inplace=True)
-    chinese['overview'] = chinese['overview'].apply(clean)
+    chinese['clean_overview'] = chinese['overview'].apply(clean_text_chinese)
+    chinese['clean_overview'] = chinese['clean_overview'].apply(partial(remove_stopwords, language='chinese'))
+    english['clean_overview'] = english['overview'].apply(clean_text_english)
+    english['clean_overview'] = english['clean_overview'].apply(partial(remove_stopwords, language='english'))
     chinese['id'] = chinese['id'].apply(partial(specialize, language='chinese'))
     english['id'] = english['id'].apply(partial(specialize, language='english'))
-    chinese['genres'] = chinese['genres'].apply(chinese_genre_transform)
-    english['genres'] = english['genres'].apply(english_genre_transform)
+    chinese['genres_new'] = chinese['genres'].apply(chinese_genre_transform)
+    english['genres_new'] = english['genres'].apply(english_genre_transform)
     df = pd.concat([english, chinese], ignore_index=True)
     df.drop('image', axis=1, inplace=True)
 
     mlb = MultiLabelBinarizer()
-    one_hot_coding = mlb.fit_transform(df['genres'].tolist())
+    one_hot_coding = mlb.fit_transform(df['genres_new'].tolist())
     res = pd.DataFrame(one_hot_coding, columns=mlb.classes_)
     concatenated_df = pd.concat([df, res], axis=1)
 
